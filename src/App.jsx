662978f5f19a2894e11882cf571c05435e74e9c8
Ragnarok794 <adrianizq94@gmail.com>
2023-11-06 23:12:52 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './App.css'
+
+import Pokedex from './components/pokedex'
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+    <Pokedex />
+
+      </div>
+    </>
+  )
+}
+
+export default App
